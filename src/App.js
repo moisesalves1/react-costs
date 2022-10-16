9b85589c11f203e025b07c935d13d370e7d74dfc
moisesalves1 <moisesalves1@gmail.com>
2022-10-16 11:14:05 -0300

    Criando app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <p>Primeiro app React</p>
+      <span>Moises</span>
+    </div>
+  );
+}
+
+export default App;
