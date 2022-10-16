7bae0f7184e17437f3b4875bfbd5b0e945c757b1
moisesalves1 <moisesalves1@gmail.com>
2022-10-16 11:23:54 -0300

    Entendendo o JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
 function App() {
+  const name = 'Moises'
+  const newName = name.toUpperCase();
+
+  function sum(a, b){
+    return a + b
+  }
+
+  const url = 'https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_720/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/t9ur9cc1khkup1dmcbzd/IMGWorldsofAdventure.webp'
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Primeiro app React</p>
-      <span>Moises</span>
+      <h1>Olá, {newName}</h1>
+      <p>Soma: {sum(1, 5)}</p>
+      <img src={url} alt='teste'></img>
     </div>
   );
 }
