f5062fec16e124470a16b0a91dc102c3cacbeec8
moisesalves1 <moisesalves1@gmail.com>
2022-10-17 08:16:58 -0300

    Usando props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld.js';
+import Pessoa from './components/Pessoa';
+import SayMyName from './components/SayMyName';
 
 function App() {
-  const name = 'Moises'
-  const newName = name.toUpperCase();
-
-  function sum(a, b){
-    return a + b
-  }
-
-  const url = 'https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_720/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/t9ur9cc1khkup1dmcbzd/IMGWorldsofAdventure.webp'
+  const name = 'Moises Alves'
 
   return (
     <div className="App">
-      <h1>Olá, {newName}</h1>
-      <p>Soma: {sum(1, 5)}</p>
-      <img src={url} alt='teste' width='150' height='150'></img>
-      <HelloWorld/>
+      <SayMyName nome={name}/>
+      <Pessoa 
+        nome="Moises"
+        idade="24"
+        profissao="Programador"
+        foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDTTKabtgp5U6vNioZmPQVzc7Vcl30lAD-Pg&usqp=CAU"/>
     </div>
   );
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SayMyName (props) {
+    return (
+        <div>
+            <h1>Fala aí {props.nome}, suave?</h1>
+        </div>
+    )
+}
+
+export default SayMyName
